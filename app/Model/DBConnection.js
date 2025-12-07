194d4cee9a8c5f2e52f7375080a52f83d9fec1f9
talarivinod <tvndkumar@gmail.com>
2025-12-07 15:46:11 +0530

    updatedd
    Signed-off-by: talarivinod <tvndkumar@gmail.com>

--- app/Model/DBConnection.js
+++ app/Model/DBConnection.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 const config = require('../Configfiles/config.json');
+
 var dbConnection = {
   connectToDB: function connectWithDatabase() {
     console.log("Called Database connection");
+    
     mongoose.set('strictQuery', true);
-    //mongoose.set('useCreateIndex', true);
-    mongoose.connect(config.connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function callback() {  
-      console.log("Mongodb is Connected");
-    });
+
+    mongoose.connect(config.connectionString)
+      .then(() => {
+        console.log("Mongodb is Connected");
+      })
+      .catch((err) => {
+        console.error("MongoDB connection error:", err);
+      });
   }
 }
+
 module.exports = dbConnection;
-
-
